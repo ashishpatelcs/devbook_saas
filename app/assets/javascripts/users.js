cbb55317ed6aeb0e0b6485f0d287e9857bb286e9
Ashish Patel <ashish.tripleclicks@gmail.com>
2017-03-30 20:09:43 +0000

    Added stripe js

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+   Stripe.setPublishableKey($('meta[name="stripe-key"]').attr("content"));
+   // watch for a form submission
+   $('.form-submit-btn').click(function(event) {
+       event.preventDefault();
+       $('input[type=submit]').prop('disabled', true);
+       var error = false;
+       var ccNum = $('#card_number').val();
+       var cvcNum = $('#card_code').val();
+       var expMonth = $('#card_month').val();
+       var expYear = $('#card_year').val();
+       
+       if (!error) {
+           // Get the Stripe token:
+           Stripe.createToken({
+               number: ccNum,
+               cvc: cvcNum,
+               exp_month: expMonth,
+               exp_year: expYear
+           }, stripeResponseHandler);
+       }
+       return false;
+   });
+   
+   function stripeResponseHandler(status, reponse) {
+       // Get a reference to the fase:
+       var f = $('#new_user');
+       var token = response.id;
+       f.append("<input type='hidden' name='user[stripe_card_token]' value='" + token + "' />");
+       
+       f.get(0).submit();
+   }
+});
